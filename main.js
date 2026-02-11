8bc5ba791891805c7043d9f1a124638290970606
nastaranrezaei99 <nastaran.rezaei@stud.hs-bochum.de>
2026-02-11 15:49:26 +0100

    0

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const landing = document.getElementById("landing");
 const landing2 = document.getElementsById ("step1");
 const  form2 = document.getElementById("secque");     
 const moodForm = document.getElementById("firstque");   
-const btn1 = document.getElementById("second-Button"); 
+const btnMood  = document.getElementById("second-Button"); 
 const btn2 = document.getElementById ("button1");
 
 
-btn1.addEventListener("click", () => {
+btnMood.addEventListener("click", () => {
   
   landing.style.display = "none";
 
