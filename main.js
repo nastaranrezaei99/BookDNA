cfc36929319f050e4a9ab3cdf2590a4d337a7c09
nastaranreezaei <nastaranrezaei99@gmail.com>
2026-05-16 16:11:11 +0200

    final version fuer fragen

---
+++ main.js
@@ -0,0 +1,57 @@
+const form1 = document.getElementById("firstque");
+const button1 = document.getElementById("button1");
+const form2 = document.getElementById("secque");
+const form3 = document.getElementById("thirdque");
+const form4 = document.getElementById("f4que");
+const form5 = document.getElementById("f5que");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const back1 = document.getElementById("button22");
+const back2 = document.getElementById("button32");
+const back3 = document.getElementById("button42");
+const back4 = document.getElementById("button52");
+
+
+button1.addEventListener("click", () => {
+    form1.style.display = "none";
+    form2.style.display = "block";
+});
+
+back1.addEventListener("click", () => {
+    form2.style.display = "none";
+    form1.style.display = "block";
+});
+
+
+button2.addEventListener("click", () => {
+    form2.style.display = "none";
+    form3.style.display = "block";
+});
+
+back2.addEventListener("click", () => {
+    form3.style.display = "none";
+    form2.style.display = "block";
+});
+
+
+button3.addEventListener("click", () => {
+    form3.style.display = "none";
+    form4.style.display = "block";
+});
+
+back3.addEventListener("click", () => {
+    form4.style.display = "none";
+    form3.style.display = "block";
+});
+
+
+button4.addEventListener("click", () => {
+    form4.style.display = "none";
+    form5.style.display = "block";
+});
+
+back4.addEventListener("click", () => {
+    form5.style.display = "none";
+    form4.style.display = "block";
+});
